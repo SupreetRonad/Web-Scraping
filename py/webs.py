bb029c3920619385f4efe5ec5618910944ca4121
SupreetRonad <62396592+SupreetRonad@users.noreply.github.com>
2021-06-19 21:40:18 +0530

    Getting price and land size done

--- py/webs.py
+++
@@ -1,7 +0,0 @@
-import bs4
-import requests
-
-print('Hello world')
-# res = requests.get('https://www.propertiesguru.com/residential-search/2bhk-residential_apartment_flat-for-sale-in-new_delhi')
-# print(res.text)
-# print('hey')
--- webs.py
+++ webs.py
@@ -1,4 +1,16 @@
 import bs4
 import requests
 
-res = requests.get('https://www.propertiesguru.com/residential-search/2bhk-residential_apartment_flat-for-sale-in-new_delhi')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+res = requests.get('https://www.propertiesguru.com/residential-search/2bhk-residential_apartment_flat-for-sale-in-new_delhi', headers=headers)
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+title = soup.select('.filter-pro-info')
+for i in soup.select('.filter-pro-info'):
+    temp = i.select('.property-price')
+    print(temp[0].text) 
+    print('\n ########################## \n')
+
